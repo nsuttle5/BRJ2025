2bd2ecb58630d0195c2334528318bd1d495c78d5
Aniket Yadav <yaniketofficial@gmail.com>
2025-01-03 21:29:34 +0530

    ...

--- Assets/Scripts/UI/CardsUI/CardScripts/TheFoolCard.cs
+++ Assets/Scripts/UI/CardsUI/CardScripts/TheFoolCard.cs
@@ -7,22 +7,31 @@ public class TheFoolCard : Card
     [SerializeField] private float buffDuration;
 
     private float defaultMoveSpeed;
+    private GameObject thisGameObject;
+
+    private void Awake()
+    {
+        Debug.Log("Here");
+        thisGameObject = gameObject;
+        Debug.Log(gameObject);
+    }
 
     public override void UseCard()
     {
         //Reduce move speed for certain duration
         defaultMoveSpeed = PlayerManager.Instance.GetMoveSpeed();
         PlayerManager.Instance.SetMoveSpeed(PlayerManager.Instance.GetMoveSpeed() * moveSpeedMultiplier);
-        //StartCoroutine(BuffDuration());
+        StartCoroutine(BuffDuration());
+        Debug.Log("IS ACTIVE? " + gameObject.activeSelf);
 
         //Increase health by one
         PlayerManager.Instance.Heal(1);
     }
 
-    //private IEnumerator BuffDuration()
-    //{
-    //    yield return new WaitForSeconds(buffDuration);
-    //    PlayerManager.Instance.SetMoveSpeed(defaultMoveSpeed);
-    //    Destroy(gameObject);
-    //}
+    private IEnumerator BuffDuration()
+    {
+        yield return new WaitForSeconds(buffDuration);
+        PlayerManager.Instance.SetMoveSpeed(defaultMoveSpeed);
+        Destroy(thisGameObject);
+    }
 }
